cc06d6246e3d55ad76f7ab64de03c996c7abbec6
CodeKhaos <goodkhaos@gmail.com>
2024-05-05 19:04:38 -0700

    trying to fix ably

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
@@ -8,11 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import * as Ably from 'ably';
-import { AblyProvider, ChannelProvider } from 'ably/react';import {
-  GetSecretValueCommand,
-  SecretsManagerClient,
-} from "@aws-sdk/client-secrets-manager";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,44 +16,17 @@ const queryClient = new QueryClient({
     }
   }
 })
-const [ablyKey, setAblyKey] = useState<string>()
 
-  const getSecretValue = async (secretName = "ABLY_KEY") => {
-    const client = new SecretsManagerClient();
-    const response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secretName,
-      }),
-    );
-    console.log(response);
-  
-    if (response.SecretString) {
-      setAblyKey(response.SecretString);
-
-    }
-  
-    if (response.SecretBinary) {
-      return response.SecretBinary;
-    }
-  };
-  
-useEffect(() => {
-  getSecretValue()
-}, [])
 const googleOAuthClientId = '1015986740737-bnii3vuh7eond8v9uj5fg3le18gs9i6r.apps.googleusercontent.com'
 
-const ablyClient = new Ably.Realtime({ key: ablyKey });
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={googleOAuthClientId}>
-    <AblyProvider client={ablyClient}> 
       <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-          <ChannelProvider channelName="rewards">
             <App />
-          </ChannelProvider>
         </QueryClientProvider>
       </React.StrictMode>
-    </AblyProvider>
   </GoogleOAuthProvider>
 )
