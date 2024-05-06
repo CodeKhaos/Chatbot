57e1883c1b970b529102bef4d7e6d7910a52f44e
CodeKhaos <goodkhaos@gmail.com>
2024-05-05 17:45:48 -0700

    ablky secret key

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+
+import { secret } from '@aws-amplify/backend';
 import './index.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,8 +10,10 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import * as Ably from 'ably';
-import { AblyProvider, ChannelProvider } from 'ably/react';
-//require('dotenv').config()
+import { AblyProvider, ChannelProvider } from 'ably/react';import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,9 +23,28 @@ const queryClient = new QueryClient({
   }
 })
 
+  const getSecretValue = async (secretName = "ABLY_KEY") => {
+    const client = new SecretsManagerClient();
+    const response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secretName,
+      }),
+    );
+    console.log(response);
+  
+    if (response.SecretString) {
+      return response.SecretString;
+    }
+  
+    if (response.SecretBinary) {
+      return response.SecretBinary;
+    }
+  };
+  
+
 const googleOAuthClientId = '1015986740737-bnii3vuh7eond8v9uj5fg3le18gs9i6r.apps.googleusercontent.com'
 
-const ablyClient = new Ably.Realtime({ authUrl: 'https://4vwaz2516d.execute-api.us-east-2.amazonaws.com/ably/ablyAuth' });
+const ablyClient = new Ably.Realtime({ key: await getSecretValue().toString() });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={googleOAuthClientId}>
