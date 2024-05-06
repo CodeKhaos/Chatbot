c60d85b4d58ee1e6b82b4baf288b1a10f20a7650
CodeKhaos <goodkhaos@gmail.com>
2024-05-06 11:07:02 -0700

    updating to serverless express for netifly

--- server/server.js
+++ server/server.js
@@ -1,22 +1,17 @@
-import express from 'express'
+import express, {Router} from 'express'
 import cors from 'cors'
 import rewards from '../server/routes/rewards.js'
 import redemptions from '../server/routes/redemptions.js'
 import ably from '../server/routes/ably.js'
 
-import dotenv from 'dotenv'
-const app = express(); 
-dotenv.config()
-const port =  5050;
+import serverless from "serverless-http";
 
-app.use(cors());
-app.use(express.json());
-app.use("/rewards", rewards);
-app.use("/redemptions", redemptions);
-app.use("/ably", ably);
+const api = express(); 
 
-app.listen(port, () => { 
-  console.log(`Server is running on port ${port}`); 
-});
+const router = Router();
 
-export default app;
+api.use("/rewards/", router);
+api.use("/redemptions/", router);
+api.use("/ably/", router);
+
+export default handler = serverless(api);
--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ const queryClient = new QueryClient({
 
 const googleOAuthClientId = '1015986740737-bnii3vuh7eond8v9uj5fg3le18gs9i6r.apps.googleusercontent.com'
 
-const ablyClient = new Ably.Realtime({ authUrl: 'http://localhost:5050/ably/auth' });
+const ablyClient = new Ably.Realtime({ authUrl: '/ably/auth' });
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={googleOAuthClientId}>
     <AblyProvider client={ablyClient}> 
